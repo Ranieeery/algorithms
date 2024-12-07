cd5e5a191b7a5807a7e772fe5614126c79ff60a1
Ranieeery <raniery2003@hotmail.com>
2024-12-07 12:54:52 -0300

    refactor: replace BufferedReader with Files.readAllLines for improved file reading in Day1.java

--- Day_01/Day1.java
+++ Day_01/Day1.java
@@ -1,33 +1,27 @@
 package Day_01;
-
-import java.io.BufferedReader;
-import java.io.FileReader;
+    
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 public class Day1 {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
         ArrayList<Integer> collumn1 = new ArrayList<>();
         ArrayList<Integer> collumn2 = new ArrayList<>();
 
-        try (BufferedReader br = new BufferedReader(new FileReader("Day_01//input.txt"))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] parts = line.trim().split("\\s+");
-                if (parts.length == 2) {
-                    collumn1.add(Integer.parseInt(parts[0]));
-                    collumn2.add(Integer.parseInt(parts[1]));
-                }
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        List<String> input = Files.readAllLines(Path.of("Day_01//input.txt"));
+        input.forEach(line -> {
+            String[] parts = line.trim().split("\\s+");
+            collumn1.add(Integer.parseInt(parts[0]));
+            collumn2.add(Integer.parseInt(parts[1]));
+        });
 
         Collections.sort(collumn1);
         Collections.sort(collumn2);
-        
+
         // Part 1
         int totalPart1 = 0;
         for (int i = 0; i < collumn1.size(); i++) {
