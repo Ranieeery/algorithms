a7c41c4ab9d80b24e81709b212161c73c047e8f3
Ranieeery <raniery2003@hotmail.com>
2024-12-07 17:44:02 -0300

    feat: confesso que estou surpreendido pelo Matcher e Pattern

--- Day_03/Day3.java
+++ Day_03/Day3.java
@@ -10,17 +10,40 @@ public class Day3 {
     public static void main(String[] args) throws IOException {
         String input = Files.readString(Path.of("Day_03/input.txt"));
 
-        Matcher matcher = Pattern.compile("mul\\((\\d{1,3}),(\\d{1,3})\\)").matcher(input);
+        Matcher matcher = Pattern.compile("(mul\\((\\d{1,3}),(\\d{1,3})\\))|(do\\(\\))|(don't\\(\\))").matcher(input);
 
         System.out.println(mulFinder(matcher));
+        matcher.reset();
+        System.out.println(mulEnabler(matcher));
     }
 
     public static int mulFinder(Matcher matcher) {
         int total = 0;
         while (matcher.find()) {
-            int x = Integer.parseInt(matcher.group(1));
-            int y = Integer.parseInt(matcher.group(2));
-            total += x * y;
+            if (matcher.group(1) != null) {
+                int x = Integer.parseInt(matcher.group(2));
+                int y = Integer.parseInt(matcher.group(3));
+                total += x * y;
+            }
+        }
+        return total;
+    }
+
+    public static int mulEnabler(Matcher matcher) {
+        int total = 0;
+        boolean control = true;
+        while (matcher.find()) {
+            if (matcher.group(1) != null) {
+                if (control) {
+                    int x = Integer.parseInt(matcher.group(2));
+                    int y = Integer.parseInt(matcher.group(3));
+                    total += x * y;
+                }
+            } else if (matcher.group(4) != null) {
+                control = true;
+            } else if (matcher.group(5) != null) {
+                control = false;
+            }
         }
         return total;
     }
