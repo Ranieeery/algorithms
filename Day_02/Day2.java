0aa3b67ed543165d549ac9fae0062f1a59c5096b
Ranieeery <raniery2003@hotmail.com>
2024-12-07 14:31:25 -0300

    refactor: replace BufferedReader with Files.readAllLines for improved file reading and add safety check method for enhanced functionality in Day2.java

--- Day_02/Day2.java
+++ Day_02/Day2.java
@@ -1,30 +1,28 @@
 package Day_02;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 
 public class Day2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        int safe = 0;
-        try (BufferedReader br = new BufferedReader(new FileReader("Day_02//input.txt"))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] parts = line.trim().split("\\s+");
-                int[] numbers = new int[parts.length];
+        int safe = 0, safe2 = 0;
+        List<String> input = Files.readAllLines(Path.of("Day_02//input.txt"));
+        for (String line : input) {
+            String[] parts = line.trim().split("\\s+");
+            int[] numbers = new int[parts.length];
 
-                for (int i = 0; i < parts.length; i++) {
-                    numbers[i] = Integer.parseInt(parts[i]);
-                }
+            for (int i = 0; i < parts.length; i++) numbers[i] = Integer.parseInt(parts[i]);
 
-                if (isSafe(numbers)) safe++;
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+            if (isSafe(numbers)) safe++;
+
+            if (isSafeRemovingLevel(numbers)) safe2++;
         }
 
         System.out.println(safe);
+        System.out.println(safe2);
     }
 
     public static boolean isSafe(int[] numbers) {
@@ -43,5 +41,21 @@ public class Day2 {
             if (!increasing && !decreasing) return false;
         }
         return true;
+    }
+
+    public static boolean isSafeRemovingLevel(int[] numbers) {
+        if (isSafe(numbers)) return true;
+
+        for (int i = 0; i < numbers.length; i++) {
+            int index = 0;
+            int[] temp = new int[numbers.length - 1];
+
+            for (int j = 0; j < numbers.length; j++) {
+                if (j != i) temp[index++] = numbers[j];
+            }
+
+            if (isSafe(temp)) return true;
+        }
+        return false;
     }
 }
