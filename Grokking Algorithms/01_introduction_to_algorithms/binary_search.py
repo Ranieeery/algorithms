b0a1f2aab0861ecaf5255f8aa908c196060afc06
Ranieeery <raniery2003@hotmail.com>
2025-02-16 15:56:51 -0300

    feat: implement binary search algorithm in binary_search.py

---
+++ Grokking Algorithms/01_introduction_to_algorithms/binary_search.py
@@ -0,0 +1,24 @@
+def binary_search(list, item):
+
+    min = 0
+    max = len(list) - 1
+
+    while min <= max:
+        mid = (min + max) // 2
+        guess = list[mid]
+
+        if guess == item:
+            return mid
+
+        if guess > item:
+            max = mid - 1
+        else:
+            min = mid + 1
+
+    return None
+
+my_list = [15, 27, 39, 77, 92, 108, 121, 135, 200, 432, 12, 8, 1, 0]
+my_list.sort()
+
+print(my_list)
+print(binary_search(my_list, 77))
