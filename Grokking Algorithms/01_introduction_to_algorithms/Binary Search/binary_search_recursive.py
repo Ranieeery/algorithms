b34214686a609d1c28c56ee45c084929bea8bc8e
Ranieeery <raniery2003@hotmail.com>
2025-02-16 18:52:02 -0300

    feat: add recursive binary search implementation in binary_search_recursive.py

---
+++ Grokking Algorithms/01_introduction_to_algorithms/Binary Search/binary_search_recursive.py
@@ -0,0 +1,28 @@
+def binary_search(list, item, min, max):
+
+    if min <= max:
+        mid = (min + max) // 2
+        guess = list[mid]
+
+        if guess == item:
+            return mid
+
+        if guess > item:
+            max = mid - 1
+            return binary_search(list, item, min, max)
+        else:
+            min = mid + 1
+            return binary_search(list, item, min, max)
+    else:
+        return None
+    
+
+
+my_list = [15, 27, 39, 77, 92, 108, 121, 135, 200, 432, 12, 8, 1, 0]
+my_list.sort()
+
+min = 0
+max = len(my_list) - 1
+
+print(my_list)
+print(binary_search(my_list, 77, min, max))
