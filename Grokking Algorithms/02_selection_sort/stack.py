60acdfa68f6259ffc54cb1991084e339bd7ba490
Ranieeery <raniery2003@hotmail.com>
2025-02-25 22:28:19 -0300

    feat: add greeting functions in stack.py for personalized salutations

---
+++ Grokking Algorithms/02_selection_sort/stack.py
@@ -0,0 +1,13 @@
+def salute(name):
+    print("Hi, " + name + "!")
+    salute2(name)
+    print("Getting ready to say bye...")
+    bye()
+
+def salute2(name):
+    print("How are you, " + name + "?")
+
+def bye():
+    print("OK, bye!")
+
+salute("Raniery")
